673b2f07f1a23bc90cd600ccf5844efea762c52c
Killian <mcevoyki@gmail.com>
2021-01-21 19:32:07 +0000

    End of section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { HomeComponent } from './home/home.component';
@@ -6,6 +8,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 
 const routes: Routes = [
@@ -21,7 +24,10 @@ const routes: Routes = [
       {path: 'messages', component: MessagesComponent},
     ]
   },
-  {path: '**', component: HomeComponent, pathMatch: 'full'}
+  {path: 'errors', component: TestErrorsComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: '**', component: NotFoundComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,18 +13,14 @@ export class NavComponent implements OnInit {
   model: any = {};
 
   constructor(public accountService: AccountService, private router: Router,
-    private toastr: ToastrService) { }
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
   login(){
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error => {
-      // console.log(error);
-      this.toastr.error(error.error);
-    }
-    );
+    });
   }
   logout() {
     this.accountService.logout();
